e4cb4dfdb425fe11d15329604971f4815b242d76
wei-elementary <haoyifang@doctor.com>
2021-06-30 17:04:37 +0800

    使用antd的表单设计

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import FormPage from './pages/FormPage';
 import HocPage from './pages/HocPage';
 
 function App() {
   return (
     <div className="App">
       <HocPage/>
-
-      
+      <FormPage/>
     </div>
   );
 }
